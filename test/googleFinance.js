3660234a708897c62886db2bb61ca693ac5c430f
Karl Stoney <kstoney@thoughtworks.com>
2016-07-01 18:38:12 +0100

    Ability to query google finance for stock data

---
+++ test/googleFinance.js
@@ -0,0 +1,34 @@
+'use strict';
+let GoogleFinance = require('../lib/markets').Google;
+let assert = require('assert');
+
+describe('Google Finance', () => {
+  let googleFinance = new GoogleFinance();
+
+  it('should get the current data for a single symbol', (done) => {
+    googleFinance.quote('LON:VM', (err, result) => {
+      assert.ifError(err);
+      result.ticker.should.eql('VM');
+      result.exchange.should.eql('LON');
+      done();
+    });
+  });
+
+  it('should let me get the current data for multiple symbols', (done) => {
+    googleFinance.quote(['LON:VM', 'LON:BARC'], (err, result) => {
+      assert.ifError(err);
+      result.length.should.eql(2);
+      let vm = result[0];
+      let barc = result[1];
+
+      vm.ticker.should.eql('VM');
+      vm.exchange.should.eql('LON');
+      barc.ticker.should.eql('BARC');
+      barc.exchange.should.eql('LON');
+
+      done();
+    });
+
+  });
+
+});
